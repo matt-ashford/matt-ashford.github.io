6dccfb6f73263e098bbf862a3cb60a4c83b7fab9
Ashford <Matthew.Ashford@prc.gov>
2024-04-05 10:32:03 -0400

    sprading targets across quarterly data

--- src/DashComponents/LineGraphProduct/FilterDataLineGraph.js
+++ src/DashComponents/LineGraphProduct/FilterDataLineGraph.js
@@ -4,17 +4,52 @@ export const FilterDataLineGraph = (
   joinedDataQtr
 ) => {
   const dataQtrProduct = joinedDataQtr.filter((row) => {
-    // return row.product_id === 92;
     return row.product_id === selectedProductId;
   });
   const dataAnnualProduct = joinedDataAnnual.filter((row) => {
-    // return row.product_id === 92;
     return row.product_id === selectedProductId;
   });
 
-  const rez = dataQtrProduct;
+  let graphData = dataQtrProduct;
+  let isUsingAnnual = false;
 
-  console.log(dataQtrProduct);
+  if (dataQtrProduct.length === 0) {
+    graphData = dataAnnualProduct;
+    isUsingAnnual = true;
+  }
+
+  if (!isUsingAnnual) {
+    graphData = graphData.map((row) => {
+      const { target, ...rest } = row;
+
+      const matchingFY = rest.fy;
+      const annualDataRows = dataAnnualProduct.filter((filtRow) => {
+        return filtRow.fy === matchingFY;
+      });
+
+      const firstRow = annualDataRows[0];
+      //   return firstRow;
+      //   return Object.keys(firstRow); // returns ['target', 'product']
+      //   return firstRow.product; // returns the product names
+      return { target: firstRow.target, ...rest }; // returns null. it should not be null.
+
+      //   const annualTarget = annualDataRow["target"];
+      //   return { annualDataRow['annualDataRow']['target'] };
+      //   return { annualDataRow[annualDataRow] };
+      //   return { annualDataRow };
+      //   return firstRow.target;
+      //   return firstRow;
+      //   return firstRow.product;
+      //   return Object.keys(firstRow);
+      return firstRow["target"];
+      //   return { target: annualTarget, ...row };
+      //   return { fy: matchingFY, target: annualTarget };
+    });
+  }
+
+  //   const rez = dataQtrProduct;
+
+  console.log(graphData);
   //   console.log(joinedDataQtr);
   //   console.log(joinedDataAnnual);
 
